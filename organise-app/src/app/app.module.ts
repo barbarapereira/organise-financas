fb931d6504e9bd0dae41b78df96da9f1ec980e50
Barbara Pereira <barbara.pereira1990@gmail.com>
2022-08-20 20:52:08 -0300

    criando footer

--- organise-app/src/app/app.module.ts
+++ organise-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
